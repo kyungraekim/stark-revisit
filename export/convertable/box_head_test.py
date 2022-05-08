6e1227d0fc41cd8a6f93def931a95fb968f6f3e9
kyungraekim <krkim741@gmail.com>
2022-05-09 00:33:44 +0900

    Add tflite converting test in box_head_test.py

--- export/convertable/box_head_test.py
+++ export/convertable/box_head_test.py
@@ -22,3 +22,21 @@ class BoxHeadTest(test_utils.DualModelTest):
         tf_model.import_torch_model(torch_model)
         box_c = tf_model(self.inputs.box_prediction(dtype=test_utils.numpy))
         self.diff_inside(box_p, box_c, self.epsilon)
+
+    def test_tflite(self):
+        _, tf_model = self.get_models()
+        tf_model(self.inputs.box_prediction(dtype=test_utils.numpy))
+        tflite_model = self.get_tf_converted(tf_model)
+        self.assertIsNotNone(tflite_model)
+
+    def test_torch_to_tflite(self):
+        torch_model, tf_model = self.get_models()
+        tf_input = self.inputs.box_prediction(dtype=test_utils.numpy)
+        tf_model(tf_input)
+        tf_model.import_torch_model(torch_model)
+        v_c = tf_model(self.inputs.box_prediction(dtype=test_utils.numpy))
+        tflite_model = self.get_tf_converted(tf_model)
+        self.assertIsNotNone(tflite_model)
+
+        v_lite = self.call_tflite_model(tflite_model, tf_input)[0]
+        self.diff_inside(v_c, v_lite, epsilon=1e-2)
